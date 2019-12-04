81930781c71f14ac87465c5de2c775db35012b14
pepecachivache <giuzepelin@gmail.com>
2019-12-04 19:00:09 -0300

    test config

--- app/controllers/ordersController.js
+++ app/controllers/ordersController.js
@@ -68,7 +68,7 @@ module.exports = {
         },
         results: function (req,res){
             // check ambient to send order
-            let url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/product/search-order/': 'https://ganymede.herokuapp.com/api/product/search-order/';
+            let url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/product/search-order': 'https://ganymede.herokuapp.com/api/product/search-order';
             let id = req.body._id;
             Orders.findById(id ,(err, ord) =>{
                 if(err)
--- app/routes/products.js
+++ app/routes/products.js
@@ -4,7 +4,7 @@ const ordersController = require('../controllers/ordersController')
 
 router.post('/product/search', ordersController.saveOrders);
 router.get('/product/search-order/:id', ordersController.getOrder);
-router.get('/product/search-orders/', ordersController.getAllOrders);
+router.get('/product/search-orders', ordersController.getAllOrders);
 router.get('/product/category/:category', ordersController.getByCategory);
 router.post('/orders/check', ordersController.checkOrder);
 router.post('/results', ordersController.results);
