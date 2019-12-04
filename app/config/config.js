672e762e09b0960b6a2ef386ea9e7e94cdb4d774
pepecachivache <giuzepelin@gmail.com>
2019-12-04 18:44:11 -0300

    test config

--- app/config/config.js
+++ app/config/config.js
@@ -3,7 +3,11 @@ dotenv.config();
 
 module.exports = {
     development: {
-         connectionLimit: 10,
+        connectionLimit: 10,
+        database: process.env.DATABASE || 'mongodb+srv://sirena:sirena@cluster0-vgvxp.mongodb.net/ganymede?retryWrites=true&w=majority',    
+    },
+    production:{
+        connectionLimit: 10,
         database: process.env.DATABASE || 'mongodb+srv://sirena:sirena@cluster0-vgvxp.mongodb.net/ganymede?retryWrites=true&w=majority',    
     }
 }
